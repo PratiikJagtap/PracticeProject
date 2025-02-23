a991ca250ff19e9bfa224736ff2d288def108db2
Pratik Jagtap <pratikjagtapofficial@gmail.com>
2025-02-24 02:17:44 +0530

    Project started

---
+++ Core/home/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def home(request):
+    peoples = [
+        {'Name' : 'Pratik jagtap', 'Age' : 22},
+        {'Name' : 'Pratiksha Jagtap', 'Age' : 19},
+        {'Name' : 'Komal Kolate', 'Age' : 22},
+        {'Name' : 'Anand Shinde', 'Age' : 15},
+        {'Name' : 'Rohit Sharma', 'Age' : 45}
+        
+    ]
+
+    return render(request , "index.html", context={'peoples' : peoples})
+
+
+
